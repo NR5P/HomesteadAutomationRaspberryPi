9a73a106b0be3c8b23fdb7a36b37ddbc45b817fb
NR5P <orangev8zcar@gmail.com>
2019-07-13 23:03:46 -0500

    moved files to folder to organize

--- cycleIrrigation.cpp
+++
@@ -1,8 +0,0 @@
-#include "cycleIrrigation.h"
-
-CycleIrrigation::CycleIrrigation(int id, const std::string &name, int pin, const std::vector<std::string> &notes, 
-const time_t &whenCreated, time_t cycleOnTime, time_t cycleOffTime, time_t blackoutStartTime, 
-        time_t blackoutStopTime) 
-    : Controller(id, name, pin, notes, whenCreated), cycleOnTime(cycleOnTime), cycleOffTime(cycleOffTime),
-    blackoutStartTime(blackoutStartTime), blackoutStopTime(blackoutStopTime)
-{}
--- main.cpp
+++
@@ -1,5 +0,0 @@
-#include "controller.h"
-
-int main() {
-    return 0;
-}
